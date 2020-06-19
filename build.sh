a39d398d477cf0e9561998bcb2c2c6a486f0c38b
ittou <VYG07066@gmail.com>
2020-06-19 17:56:36 +0900

    image name bugfix

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 IPADDRESS=$(ip addr show $( netstat -rn | grep UG | awk -F' ' '{print $8}')\
     | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' \
     | grep -o [0-9].*)
+VER=2020.1
 docker image build \
     --rm \
     --build-arg IP=${IPADDRESS} \
-    --build-arg XILINX_VER=2020.1 \
+    --build-arg XILINX_VER=${VER} \
     --build-arg XILINX_MAIN=Xilinx_Unified_2020.1_0602_1208.tar.gz \
     --no-cache \
-    -t xilinx${XILINX_VER} .
+    -t xilinx${VER} .
